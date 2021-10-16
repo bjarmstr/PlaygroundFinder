59bd67e076f994b883189a70de1741a8ab2547e2
Jean Armstrong <bjarmstr@yahoo.com>
2021-10-16 16:19:30 -0600

    added Migration

---
+++ PlaygroundFinder.Repositories/Migrations/20211016221529_PlaygroundTable.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NetTopologySuite.Geometries;
+
+namespace PlaygroundFinder.Repositories.Migrations
+{
+    public partial class PlaygroundTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:postgis", ",,");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Playgrounds",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Accessible",
+                table: "Playgrounds",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "AgeRange",
+                table: "Playgrounds",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Point>(
+                name: "GeoLocation",
+                table: "Playgrounds",
+                type: "geography (point)",
+                nullable: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Quadrant",
+                table: "Playgrounds",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Size",
+                table: "Playgrounds",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Accessible",
+                table: "Playgrounds");
+
+            migrationBuilder.DropColumn(
+                name: "AgeRange",
+                table: "Playgrounds");
+
+            migrationBuilder.DropColumn(
+                name: "GeoLocation",
+                table: "Playgrounds");
+
+            migrationBuilder.DropColumn(
+                name: "Quadrant",
+                table: "Playgrounds");
+
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "Playgrounds");
+
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:PostgresExtension:postgis", ",,");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Playgrounds",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}

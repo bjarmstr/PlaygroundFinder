8bab4ba9e0e74055d2685e78df18322176d3a90b
Jean Armstrong <bjarmstr@yahoo.com>
2021-10-18 10:17:06 -0600

    Update Migrations automatically by modifying Program.cs

--- PlaygroundFinder.API/Program.cs
+++ PlaygroundFinder.API/Program.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PlaygroundFinder.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,17 +14,40 @@ namespace PlaygroundFinder.API
 {
     public class Program
     {
+       
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+        var host = CreateHostBuilder(args).Build();
 
+        // Create a Service Scope so we can get a services collection
+        using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;  // services collection
+                try
+                {
+                    // Get an ApplicationDbContext instance so we can perform the migrations on it
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+
+                    // Perform the migrations
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    // Output an error log to the configured logging service
+                    // By default the logging service just outputs to the console
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while migrating the database.");
+                }
+            }
+
+        host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
     }
 }
